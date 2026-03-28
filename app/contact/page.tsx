d24c1f9d4eb239f91071624e38d74626d886a344
DeveloperFinzolo <developerfinzolo@gmail.com>
2026-03-28 23:40:30 +0530

    update: init commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '../../components/ContactForm'
+
+export default function Contact() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Contact Us</h1>
+      <ContactForm />
+    </div>
+  )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
-    <div className="flex justify-between p-4 shadow">
-      <h1>Financ</h1>
-      <div>
-        <a href="/">Home</a> | 
-        <a href="/about">About</a> | 
+    <div className="flex justify-between items-center py-4 px-6  shadow">
+      <Image
+        src="/logo.png"
+        alt="finzolo"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: '50px', height: 'auto' }}
+      />
+      <div className="flex flex-row gap-4">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
         <a href="/contact">Contact</a>
       </div>
+      <button>Let's Talk</button>
     </div>
   )
 }
